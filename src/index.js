336cab6ecdab4480213eae1c5e62b0e1f400885b
mepripri <mepripri0712@gmail.com>
2021-10-03 17:26:42 +0530

    Nested Component

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import ReactDOM from "react-dom";
 const Book = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Person />
+      <Message />
     </div>
   );
+};
+
+const Person = () => {
+  return <h1>Hii, I am Priyal</h1>;
+};
+
+const Message = () => {
+  return <h3>This is a Message</h3>;
 };
 
 ReactDOM.render(<Book />, document.getElementById("root"));
